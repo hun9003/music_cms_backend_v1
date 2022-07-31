bf3fbb4731cf21cee168213fc85a7aea9259c628
hun9003 <hun9003@naver.com>
2022-07-31 10:49:23 +0900

    관리자 정보 생성, 권한 수정, 호출 기능 작성

--- src/main/java/com/music/music_cms_backend_v1/domain/admin/AdminServiceImpl.java
+++ src/main/java/com/music/music_cms_backend_v1/domain/admin/AdminServiceImpl.java
@@ -3,27 +3,38 @@ package com.music.music_cms_backend_v1.domain.admin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
+    private final AdminStore adminStore;
+    private final AdminReader adminReader;
 
     @Override
+    @Transactional
     public AdminInfo registerAdmin(AdminCommand.RegisterAdmin command) {
         System.out.println("AdminServiceImpl :: registerAdmin");
-        return null;
+        var initAdmin = command.toEntity();
+        var admin = adminStore.store(initAdmin);
+        return new AdminInfo(admin);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public AdminInfo getAdminInfo(String adminToken) {
         System.out.println("AdminServiceImpl :: getAdminInfo");
-        return null;
+        var admin = adminReader.getAdmin(adminToken);
+        return new AdminInfo(admin);
     }
 
     @Override
+    @Transactional
     public AdminInfo changePermission(AdminCommand.ChangeAdminPermission command) {
         System.out.println("AdminServiceImpl :: changePermission");
-        return null;
+        var admin = adminReader.getAdmin(command.getAdminToken());
+        admin.changePermission(command.getPermission());
+        return new AdminInfo(admin);
     }
 }
